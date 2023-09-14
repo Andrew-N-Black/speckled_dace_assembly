988fcbced3025e8a8b43b625b50db28bd07ef9b8
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-09-13 17:17:36 -0700

    Update vcfFilter.sh

--- vcfFilter.sh
+++ vcfFilter.sh
@@ -7,6 +7,6 @@ vcftools --vcf populations.snps.vcf --maf 0.05 --recode --out populations_filter
 cat GCF_029890125.1_OSU_Roscu_1.1_genomic.fna.out | tail -n +4 | awk '{print $5,$6,$7,$11}' | sed 's/ /\t/g' > repeats.bed
 
 #Remove variants that fall within annotated repeat windows
-bedtools intersect -v -a populations_filtered.recode.vcf -b repeats.bed -wa > final.vcf
+ bedtools intersect -v -a populations_filtered.recode.vcf -b repeats.bed -wa -header > final.vcf
 
 
